1d4be439de5a8e33d3585a031164fe0015cb675b
R.J.V. Bertin <rjvbertin@gmail.com>
2017-12-12 22:10:08 +0100

    add some more missing things (signalled when using QtWebEngine 5.9.1)
    Committed from host : Portia.local

--- missing_OS_functions.mm
+++ missing_OS_functions.mm
@@ -7,9 +7,12 @@
 #include <qglobal.h>
 
 #import <CoreFoundation/CoreFoundation.h>
+#import <Foundation/Foundation.h>
+#import <CoreServices/CoreServices.h>
 #import <AppKit/AppKit.h>
 
 #if QT_MAC_DEPLOYMENT_TARGET_BELOW(__MAC_10_10, __IPHONE_NA)
+
 #warning "providing LSCopyDefaultApplicationURLForURL"
 Q_CORE_EXPORT CFURLRef LSCopyDefaultApplicationURLForURL(CFURLRef inURL, LSRolesMask inRoleMask, CFErrorRef *outError)
 {
@@ -23,4 +26,30 @@ Q_CORE_EXPORT CFURLRef LSCopyDefaultApplicationURLForURL(CFURLRef inURL, LSRoles
     }
     return NULL;
 }
+
+#warning "providing NSURLQuarantinePropertiesKey"
+FOUNDATION_EXPORT NSString * const NSURLQuarantinePropertiesKey = (NSString*) kLSItemQuarantineProperties;
+
+#endif
+
+#if QT_MAC_DEPLOYMENT_TARGET_BELOW(__MAC_10_11, __IPHONE_NA)
+
+#warning "providing [NSFont systemFontOfSize:weight:]"
+
+typedef CGFloat NSFontWeight;
+
+@interface NSFont (missing)
++ (NSFont*) systemFontOfSize:(CGFloat)fontSize weight:(NSFontWeight)weight;
+@end
+
+@implementation NSFont (missing)
+
++ (NSFont*) systemFontOfSize:(CGFloat)fontSize weight:(NSFontWeight)weight
+{
+    NSFont *fnt = [NSFont systemFontOfSize:fontSize];
+    NSLog(@"[NSFont systemFontOfSize:%g weight:%g] ignoring unsupported weight param; returning font %@", fontSize, weight, fnt);
+    return fnt;
+}
+
+@end
 #endif
